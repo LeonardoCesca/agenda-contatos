ca6db963fbddfd2edad8787922b2921ae23de2e3
LeonardoCesca <leonardocescaf@gmail.com>
2019-11-24 20:34:31 -0300

    ADD change strings default

--- app/src/main/java/com/example/agenda_contatos/Formulario.java
+++ app/src/main/java/com/example/agenda_contatos/Formulario.java
@@ -2,13 +2,8 @@ package com.example.agenda_contatos;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class Formulario extends AppCompatActivity {
 
@@ -18,15 +13,6 @@ public class Formulario extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
