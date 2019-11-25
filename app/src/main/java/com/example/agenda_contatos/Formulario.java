f89d41b61cf9f180de989cc38b99f753a9496b4b
LeonardoCesca <leonardocescaf@gmail.com>
2019-11-24 23:03:26 -0300

    ADD back button into formulario activity

--- app/src/main/java/com/example/agenda_contatos/Formulario.java
+++ app/src/main/java/com/example/agenda_contatos/Formulario.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
 
 public class Formulario extends AppCompatActivity {
 
@@ -13,6 +14,22 @@ public class Formulario extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Back Button
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == android.R.id.home) {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
