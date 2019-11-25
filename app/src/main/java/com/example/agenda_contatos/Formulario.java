e50289d2c3f57e59b1150b426a4f12bac92c50f9
LeonardoCesca <leonardocescaf@gmail.com>
2019-11-25 12:34:43 -0300

    ADD Formulario class

--- app/src/main/java/com/example/agenda_contatos/Formulario.java
+++ app/src/main/java/com/example/agenda_contatos/Formulario.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,6 +21,32 @@ public class Formulario extends AppCompatActivity {
         //Back Button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Contato meuContato = new Contato();
+
+        meuContato.setNome("Leonardo");
+        meuContato.setEmail("leonardocescaf@gmail.com");
+        meuContato.setTelefone("983310534");
+        meuContato.setEndereco("Av. Farrapos,3852");
+
+        meuContato.setId(new Long(10));
+
+        FormularioHelper helper = new FormularioHelper(this);
+
+        helper.colocaNoFormulario(meuContato);
+
+        Contato meuContato2 = new Contato();
+
+        meuContato2 = helper.pegaContatoDoFormulario();
+
+        Log.i("Meu Log", meuContato2.getNome());
+        Log.i("Meu Log", meuContato2.getEmail());
+        Log.i("Meu Log", meuContato2.getTelefone());
+        Log.i("Meu Log", meuContato2.getEndereco());
+
+        Log.i("Meu Log", String.valueOf(meuContato2.getId()));
+
+        Log.i("Meu Log", meuContato2.toString());
     }
 
     @Override
