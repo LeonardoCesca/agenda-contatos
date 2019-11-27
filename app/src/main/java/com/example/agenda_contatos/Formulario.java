9c9525088ccf2fde260547527166944d8f0f5fe1
LeonardoCesca <leonardocescaf@gmail.com>
2019-11-27 13:28:22 -0300

    FIX bug into back arrow

--- app/src/main/java/com/example/agenda_contatos/Formulario.java
+++ app/src/main/java/com/example/agenda_contatos/Formulario.java
@@ -26,46 +26,9 @@ public class Formulario extends AppCompatActivity {
 
         Contato meuContato = new Contato();
 
-        meuContato.setNome("Leonardo");
-        meuContato.setEmail("leonardocescaf@gmail.com");
-        meuContato.setTelefone("983310534");
-        meuContato.setEndereco("Av. Farrapos,3852");
-
-        meuContato.setId(new Long(10));
-
-        FormularioHelper helper = new FormularioHelper(this);
-
-        helper.colocaNoFormulario(meuContato);
-
-        Contato meuContato2 = new Contato();
-
-        meuContato2 = helper.pegaContatoDoFormulario();
-
-        Log.i("Meu Log", meuContato2.getNome());
-        Log.i("Meu Log", meuContato2.getEmail());
-        Log.i("Meu Log", meuContato2.getTelefone());
-        Log.i("Meu Log", meuContato2.getEndereco());
-
-        Log.i("Meu Log", String.valueOf(meuContato2.getId()));
-
-        Log.i("Meu Log", meuContato2.toString());
-
         this.helper = new FormularioHelper(this);
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//
-//        int id = item.getItemId();
-//
-//        if(id == android.R.id.home) {
-//            this.finish();
-//        } else if (id == R.id.menu_formulario_ok) {
-//            return false;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -83,10 +46,11 @@ public class Formulario extends AppCompatActivity {
 
                 dao.close();
 
-
-
                 finish();
                 return false;
+
+            case android.R.id.home:
+                this.finish();
 
             default:
                 return super.onOptionsItemSelected(item);
