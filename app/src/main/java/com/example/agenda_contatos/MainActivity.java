e894f29c78a35f45a0db307c74750fe9220e96d8
Lucas Zoppas <lucasmzoppas@gmail.com>
2019-11-27 13:17:09 -0300

    ADD excluir contato com Alert

--- app/src/main/java/com/example/agenda_contatos/MainActivity.java
+++ app/src/main/java/com/example/agenda_contatos/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.agenda_contatos;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -40,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
 
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Contato contatoSelecionado = (Contato) minhaLista.getAdapter().getItem(info.position);
+
         final MenuItem itemLigar = menu.add("Ligar para Contato");
         final MenuItem itemSMS = menu.add("Enviar SMS");
         final MenuItem itemApagar = menu.add("Apagar Contato");
@@ -47,7 +53,24 @@ public class MainActivity extends AppCompatActivity {
         itemApagar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Apagar Contato")
+                        .setMessage("Deseja realmente apagar este contato?")
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ContatoDAO dao = new ContatoDAO(MainActivity.this);
+                                dao.apagarContato(contatoSelecionado);
+                                dao.close();
+                                carregaLista();
+                            }
+                        })
+                        .setNegativeButton("Nao", null).show();
+
+
+
                 return false;
             }
         });
