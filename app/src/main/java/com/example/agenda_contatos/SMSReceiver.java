6f6237ce57d22a53f7c587ee7a6cef1071ee9699
LeonardoCesca <leonardocescaf@gmail.com>
2019-11-28 09:54:37 -0300

    ADD receive sms and song to this

---
+++ app/src/main/java/com/example/agenda_contatos/SMSReceiver.java
@@ -0,0 +1,31 @@
+package com.example.agenda_contatos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Bundle bundle = intent.getExtras();
+        Object[] mensagens = (Object[]) bundle.get("pdus");
+
+        byte[] mensagem = (byte[])mensagens[0];
+
+        SmsMessage msg = SmsMessage.createFromPdu(mensagem);
+        String telefone = msg.getDisplayOriginatingAddress();
+
+        ContatoDAO dao = new ContatoDAO(context);
+
+        if(dao.isContato(telefone)) {
+
+            MediaPlayer player = MediaPlayer.create(context, R.raw.msg);
+            player.start();
+        }
+    }
+}
